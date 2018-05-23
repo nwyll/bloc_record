e5454f29e94a49fb5ba74639323e3f26d48369f5
Natalie Wyll <nataliewyll@yahoo.com>
2018-05-23 11:00:29 -0500

    in progress

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,13 +1,31 @@
 require 'sqlite3'
 
 module Selection
-  def find(id)
+  def find(*ids)
+    if ids.length == 1
+      find_one(ids.first)
+    else
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id IN (#{ids.join(",")});
+      SQL
+
+      rows_to_array(rows)
+    end
+  end
+
+  def find_one(id)
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE id = #{id};
     SQL
 
-    data = Hash[columns.zip(row)]
-    new(data)
+    init_object_from_row(row)
   end
-end 
+
+  private
+
+  def rows_to_array(rows)
+    rows.map { |row| new(Hash[columns.zip(row)]) }
+  end
+end
