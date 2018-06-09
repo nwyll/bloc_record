7a95621e952342835548b0e82317ed1333420311
Natalie Wyll <nataliewyll@yahoo.com>
2018-06-09 01:04:06 -0500

    checkpoint-5 complete

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -28,18 +28,19 @@ module Persistence
     true
   end
 
+  def update_attribute(attribute, value)
+    self.class.update(self.id, { attribute => value })
+  end
+
+  def update_attributes(updates)
+    self.class.update(self.id, updates)
+  end
+
   module ClassMethods
     def create(attrs)
       attrs = BlocRecord::Utility.convert_keys(attrs)
       attrs.delete "id"
-      # vals = attributes.map { |key| BlocRecord::Utility.sql_strings(attrs[key]) }
-
-      vals = attributes.each do |key|
-        puts "Key: #{key}"
-        puts "Value: #{attrs[key]}"
-        key = BlocRecord::Utility.sql_strings(attrs[key])
-      end
-
+      vals = attributes.map { |key| BlocRecord::Utility.sql_strings(attrs[key]) }
 
       connection.execute <<-SQL
         INSERT INTO #{table} (#{attributes.join ","})
@@ -49,6 +50,31 @@ module Persistence
       data = Hash[attributes.zip attrs.values]
       data["id"] = connection.execute("SELECT insert_rowid();")[0][0]
       new(data)
+    end
+
+    def update(ids, updates)
+      updates = BlocRecord::Utility.convert_keys(updates)
+      updates.delete "id"
+      updates_array = updates.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(vlaue)}" }
+
+      if ids.class == Fixnum
+        where_clause = "WHERE id = #{ids}"
+      elsif ids.class == Array
+        where_clause = ids.empty? ? ";" : "WHERE id IN (#{ids.join(",")});"
+      else
+        where_clause = ";"
+      end 
+
+      connection.execute <<-SQL
+        UPDATE #{table}
+        SET #{updates_array * ","} #{where_clause}
+      SQL
+
+      true
+    end
+
+    def update_all(updates)
+      update(nil, updates)
     end
   end
 end
