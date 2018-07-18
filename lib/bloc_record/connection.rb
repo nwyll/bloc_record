1a257338deae9e837a636c1de6d1d274b3ddcf9e
Natalie Wyll <nataliewyll@yahoo.com>
2018-07-17 20:45:28 -0500

    corrected variable assignment in connection.rb

--- lib/bloc_record/connection.rb
+++ lib/bloc_record/connection.rb
@@ -3,30 +3,29 @@ require 'pg'
 
 module Connection
   def connection
-    if @db == :sqlite3
+    if @platform == :sqlite3
       database_connection = SQLite3::Database.new(BlocRecord.database_filename)
-    elsif @db == :pg
+    elsif @platform == :pg
       database_connection = PG::Connection.new(:dbname => BlocRecord.database_filename)
     end
 
     @connection ||= database_connection
-    # @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+    #@connection ||= SQLite3::Database.new(BlocRecord.database_filename)
   end
 
   def execute(sql)
-    if @db == :sqlite3
+    if @platform == :sqlite3
       @connection.execute(sql)
-    elsif @db == :pg
+    elsif @platform == :pg
       @connection.exec(sql)
     end
   end
 
   def get_first_row(sql)
-    if @db == :sqlite3
+    if @platform == :sqlite3
       @connection.get_first_row(sql)
-    elsif @db == :pg
+    elsif @platform == :pg
       @connection.exec(sql)[0]
     end
   end
-
 end
